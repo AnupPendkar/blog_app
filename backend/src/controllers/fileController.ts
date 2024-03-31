f3cd79f7976f4e18eda7f4d89ec445679f39f812
AnupPendkar <anuppendkar@gmail.com>
2024-03-31 21:53:24 +0530

    #Add: Cloudinary for file upload

--- backend/src/controllers/fileController.ts
+++ backend/src/controllers/fileController.ts
@@ -3,48 +3,59 @@ import * as fs from 'fs';
 import * as path from 'path';
 import 'dotenv/config';
 // import { jwtClient } from '../config/upload';
-import { google } from 'googleapis';
+// const { google } = require('googleapis');
+const cloudinary = require('cloudinary').v2;
 
 export function uploadFile(req, res: Response, next: NextFunction) {
-  const jwtClient = new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.DRIVE_EMAIL,
-      private_key: process.env.DRIVE_KEY,
-    },
-    scopes: ['https://www.googleapis.com/auth/drive.file'],
-  });
-
-  const drive = google.drive({ version: 'v3', auth: jwtClient });
-
-  const fileMetadata = {
-    name: req.file.originalname,
-    parents: ['1En7zppXxhRUbj2d3JP0MaxGe8UOsCh6E'],
-    // mimeType: req.file.mimetype,
-  };
-
-  const media = {
-    mimeType: 'image/jpeg',
-    body: fs.createReadStream(req.file.path),
-  };
-
-  drive.files.create(
-    {
-      requestBody: fileMetadata,
-      media: media,
-      fields: 'id',
-    },
-    (err, file) => {
-      if (err) {
-        console.error('Error uploading image:', err);
-        res.status(500).send('Error uploading image to Google Drive');
-        return;
-      }
-
-      const imageUrl = file.data.webViewLink;
-      res.status(200).json({ imageUrl: imageUrl, imgId: file.data?.id });
-    }
-  );
+  cloudinary.uploader
+    .upload(req.file.path, {
+      resource_type: 'image',
+    })
+    .then((result) => {
+      res.status(200).send(result?.url);
+    });
 }
+
+// export function uploadFile(req, res: Response, next: NextFunction) {
+//   const jwtClient = new google.auth.GoogleAuth({
+//     credentials: {
+//       client_email: process.env.DRIVE_EMAIL,
+//       private_key: process.env.DRIVE_KEY,
+//     },
+//     scopes: ['https://www.googleapis.com/auth/drive.file'],
+//   });
+
+//   const drive = google.drive({ version: 'v3', auth: jwtClient });
+
+//   const fileMetadata = {
+//     name: req.file.originalname,
+//     parents: ['1En7zppXxhRUbj2d3JP0MaxGe8UOsCh6E'],
+//     // mimeType: req.file.mimetype,
+//   };
+
+//   const media = {
+//     mimeType: 'image/jpeg',
+//     body: fs.createReadStream(req.file.path),
+//   };
+
+//   drive.files.create(
+//     {
+//       requestBody: fileMetadata,
+//       media: media,
+//       fields: 'id',
+//     },
+//     (err, file) => {
+//       if (err) {
+//         console.error('Error uploading image:', err);
+//         res.status(500).send('Error uploading image to Google Drive');
+//         return;
+//       }
+
+//       const imageUrl = file.data.webViewLink;
+//       res.status(200).json({ imageUrl: imageUrl, imgId: file.data?.id });
+//     }
+//   );
+// }
 
 // export function uploadFile(req, res: Response, next: NextFunction) {
 //   try {
