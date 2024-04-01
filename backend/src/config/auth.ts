8c7c8297b882e8e4bb5945cecbc6b2729f56aad3
panup <anup.p@atai.ai>
2024-04-01 11:18:26 +0530

    #Change: google strategy package

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import 'dotenv/config';
 import { userRegister } from '../controllers/userControllers';
 
